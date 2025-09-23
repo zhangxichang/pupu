107f97d093d54713464f5fd36350cf00173db7c9
ZhangXiChang <zhang.xichang@outlook.com>
2025-09-23 14:20:49 +0800

    设置历史记录类型为哈希以修复刷新网页404的问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { RouterProvider, createHashHistory, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history: createHashHistory() });
 declare module "@tanstack/react-router" {
     interface Register {
         router: typeof router
