32b4c5bcc1f8453910bb0ab611246d5e11f80506
张喜昌 <zhang.xichang@outlook.com>
2026-02-10 18:35:30 +0800

    fix: 更加准确的测试表单验证

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,13 +1,21 @@
 import test, { expect } from "@playwright/test";
 
 test.describe("登录/注册界面", () => {
-  test("表单验证", async ({ page }) => {
+  test("登录表单验证", async ({ page }) => {
     await page.goto("http://127.0.0.1:8787");
     await page.getByLabel("账户选择账户").selectOption({ index: 0 });
     await page.getByRole("button", { name: "登录" }).click();
     await expect(page.getByRole("group")).toContainText("请选择一个账户");
+  });
+  test("删除用户表单验证", async ({ page }) => {
+    await page.goto("http://127.0.0.1:8787");
+    await page.getByLabel("账户选择账户").selectOption({ index: 0 });
     await page.getByRole("button", { name: "删除账户" }).click();
     await expect(page.getByRole("group")).toContainText("请选择一个账户");
+  });
+  test("注册用户表单验证", async ({ page }) => {
+    await page.goto("http://127.0.0.1:8787");
+    await page.getByLabel("账户选择账户").selectOption({ index: 0 });
     await page.getByRole("radio", { name: "注册" }).check();
     await page.getByRole("textbox", { name: "用户名" }).fill("");
     await page.getByRole("button", { name: "注册" }).click();
